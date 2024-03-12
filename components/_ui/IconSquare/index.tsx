97574106887d487d30096c8a5d000c75db246a0b
Kelvin Crepaldi <81213300+KelvinCrepaldi@users.noreply.github.com>
2024-03-11 21:22:34 -0300

    feat: add button to hidden navigation menu

---
+++ components/_ui/IconSquare/index.tsx
@@ -0,0 +1,5 @@
+const IconSquare = ({children}: {children: React.ReactNode}) =>{
+  return <div className="w-[50px] max-w-[50px] flex items-center justify-center ">{children}</div>
+}
+
+export default IconSquare
